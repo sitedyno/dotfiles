7ade63ef1ad5615a1e8a32e63ad33da2a68ad17f
Heath Nail <sitedyno@gmail.com>
2012-12-01 22:56:08 -0500

    Mispelling...

--- install/bash.sh
+++ install/bash.sh
@@ -8,13 +8,13 @@ if [[ ${EUID} == 0 ]]
 then
 	if [[ -f /root/.bashrc ]]
 	then
-		if grep -Fxq "[[ -f ~/.dotfiles/bash/bashrc ]] && ~/.dotfiles/bash/bashrc" /roob/.bashrc
+		if grep -Fxq "[[ -f ~/.dotfiles/bash/bashrc ]] && ~/.dotfiles/bash/bashrc" /root/.bashrc
 		then
 			echo "[[ -f ~/.dotfiles/bash/bashrc ]] && . ~/.dotfiles/bash/bashrc" >> /root/.bashrc
 		fi
 	elif [[ -f /root/.profile ]]
 	then
-		if grep -Fxq "[[ -f ~/.dotfiles/bash/bashrc ]] && ~/.dotfiles/bash/bashrc" /roob/.profile
+		if grep -Fxq "[[ -f ~/.dotfiles/bash/bashrc ]] && ~/.dotfiles/bash/bashrc" /root/.profile
 		then
 			echo "[[ -f ~/.dotfiles/bash/bashrc ]] && . ~/.dotfiles/bash/bashrc" >> /root/.profile
 		fi
