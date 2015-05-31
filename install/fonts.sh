a926cb519e96bba2732f6c1bbae1a0bcc83e1d1c
Heath Nail <sitedyno@gmail.com>
2015-05-31 04:59:50 -0400

    Update font links and disable wget output

--- install/fonts.sh
+++ install/fonts.sh
@@ -5,9 +5,9 @@ echo "Running fonts .dotfiles install script..."
 echo
 
 fontdir="$HOME/.fonts"
-DSMPttfLink="https://gist.github.com/raw/2324335/fafb79037ecbdd1b61ebbc512b9f5ab631a93f3b/DroidSansMono-Powerline.ttf"
-DSMDPttfLink="https://gist.github.com/raw/2324335/3f941e9b09bafdc2df960f1dd5609de0eb03533d/DroidSansMonoDotted-Powerline.ttf"
-DSMSPttfLink="https://gist.github.com/raw/2324335/af42b835b96b9c528c35c703bf13ab159c71624a/DroidSansMonoSlashed-Powerline.ttf"
+DSMPttfLink="https://gist.githubusercontent.com/shift/2324335/raw/fafb79037ecbdd1b61ebbc512b9f5ab631a93f3b/DroidSansMono-Powerline.ttf"
+DSMDPttfLink="https://gist.githubusercontent.com/shift/2324335/raw/3f941e9b09bafdc2df960f1dd5609de0eb03533d/DroidSansMonoDotted-Powerline.ttf"
+DSMSPttfLink="https://gist.githubusercontent.com/shift/2324335/raw/af42b835b96b9c528c35c703bf13ab159c71624a/DroidSansMonoSlashed-Powerline.ttf"
 which wget > /dev/null
 if [ "$?" -eq 0 ]
 then
@@ -18,19 +18,19 @@ then
 	fi
 	if [[ ! -f "$fontdir/DroidSansMono-Powerline.ttf" ]]
 	then
-		wget -O "$fontdir/DroidSansMono-Powerline.ttf" $DSMPttfLink
+		wget -O "$fontdir/DroidSansMono-Powerline.ttf" $DSMPttfLink -q
 	else
 		echo "DroidSansMono-Powerline.ttf exists, skipping download."
 	fi
 	if [[ ! -f "$fontdir/DroidSansMonoDotted-Powerline.ttf" ]]
 	then
-		wget -O "$fontdir/DroidSansMonoDotted-Powerline.ttf" $DSMDPttfLink
+		wget -O "$fontdir/DroidSansMonoDotted-Powerline.ttf" $DSMDPttfLink -q
 	else
 		echo "DroidSansMonoDotted-Powerline.ttf exists, skipping download."
 	fi
 	if [[ ! -f "$fontdir/DroidSansMonoSlashed-Powerline.ttf" ]]
 	then
-		wget -O "$fontdir/DroidSansMonoSlashed-Powerline.ttf" $DSMSPttfLink
+		wget -O "$fontdir/DroidSansMonoSlashed-Powerline.ttf" $DSMSPttfLink -q
 	else
 		echo "DroidSansMonoSlashed-Powerline.ttf exists, skipping download."
 	fi
